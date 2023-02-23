087662cce4f8511d99b1e584b6366d7acd21a703
vini00784 <vo70346@gmail.com>
2023-02-23 09:32:30 -0300

    application componentization

--- app/src/main/java/br/senai/sp/jandira/productlistds3m/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/productlistds3m/MainActivity.kt
@@ -3,22 +3,21 @@ package br.senai.sp.jandira.productlistds3m
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Card
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import br.senai.sp.jandira.productlistds3m.model.Product
+import br.senai.sp.jandira.productlistds3m.components.CreateProductCard
+import br.senai.sp.jandira.productlistds3m.dao.repository.ProductRepository
 import br.senai.sp.jandira.productlistds3m.ui.theme.ProductListDS3MTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,31 +40,22 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Listas com JetPack Compose")
+        Text(
+            text = "Lists with JetPack Compose",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
         LazyColumn(
             modifier = Modifier.padding(16.dp)
         ) {
-
-            items(10) {
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 8.dp),
-                ) {
-                    Column() {
-                        Text(
-                            text = "item ${it+1}",
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.Bold
-                        )
-                        Text(text = "item 1-${it+1}")
-                    }
-                }
+            items(ProductRepository.getProducts()) {
+                CreateProductCard(product = it)
             }
-
-
         }
     }
 }
