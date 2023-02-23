1dc3c2a0c50f72ffa5b3bbc39a2ba28eb4fae4ee
vini00784 <vo70346@gmail.com>
2023-02-23 09:31:47 -0300

    moc for tests

--- app/src/main/java/br/senai/sp/jandira/productlistds3m/dao/repository/ProductRepository.kt
+++ app/src/main/java/br/senai/sp/jandira/productlistds3m/dao/repository/ProductRepository.kt
@@ -5,12 +5,11 @@ import br.senai.sp.jandira.productlistds3m.model.Product
 class ProductRepository {
     companion object {
         fun getProducts(): List<Product> {
-            val products = listOf(
-                Product(),
-                Product(),
+            return listOf(
+                Product(id = 1, productName = "Mouse Bluetooth", productDescription = "Mouse bacana bem da hora", productPrice =  29.99),
+                Product(id =2, productName = "Caneca do coringão", productDescription = "Caneca", productPrice = 19.10),
                 Product()
             )
-            return products
         }
     }
 }
