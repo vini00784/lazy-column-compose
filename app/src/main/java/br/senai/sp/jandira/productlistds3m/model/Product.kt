7a1b5e5007023539d1c00fab69daa1467afda529
vini00784 <vo70346@gmail.com>
2023-02-23 10:51:40 -0300

    making annotations for room and database

--- app/src/main/java/br/senai/sp/jandira/productlistds3m/model/Product.kt
+++ app/src/main/java/br/senai/sp/jandira/productlistds3m/model/Product.kt
@@ -1,8 +1,14 @@
 package br.senai.sp.jandira.productlistds3m.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tbl_product")
 data class Product(
-    var id: Long = 0,
-    var productName: String = "Product's name",
-    var productDescription: String = "Description's product",
-    var productPrice: Double = 0.0
+
+    @PrimaryKey(autoGenerate = true) var id: Long = 0,
+    @ColumnInfo(name = "product_name") var productName: String = "Product's name",
+    @ColumnInfo(name = "product_description") var productDescription: String = "Description's product",
+    @ColumnInfo(name = "product_price") var productPrice: Double = 0.0
 )
