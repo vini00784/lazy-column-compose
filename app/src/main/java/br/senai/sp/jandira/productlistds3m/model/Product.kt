4d907d196462b933bb6cffef2e0bee048c0532d5
vini00784 <vo70346@gmail.com>
2023-02-23 09:32:52 -0300

    standard values for make tests

--- app/src/main/java/br/senai/sp/jandira/productlistds3m/model/Product.kt
+++ app/src/main/java/br/senai/sp/jandira/productlistds3m/model/Product.kt
@@ -2,7 +2,7 @@ package br.senai.sp.jandira.productlistds3m.model
 
 data class Product(
     var id: Long = 0,
-    var productName: String = "",
-    var productDescription: String = "",
+    var productName: String = "Product's name",
+    var productDescription: String = "Description's product",
     var productPrice: Double = 0.0
 )
