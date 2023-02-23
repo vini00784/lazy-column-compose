4e266df4f82234f0d24c8826712103f35819ea47
vini00784 <vo70346@gmail.com>
2023-02-23 10:49:37 -0300

    DAO to manipulate products data

---
+++ app/src/main/java/br/senai/sp/jandira/productlistds3m/dao/product/ProductDao.kt
@@ -0,0 +1,19 @@
+package br.senai.sp.jandira.productlistds3m.dao.product
+
+import androidx.room.*
+import br.senai.sp.jandira.productlistds3m.model.Product
+
+@Dao
+interface ProductDao {
+    @Insert
+    fun saveProduct(product: Product): Long
+
+    @Delete
+    fun deleteProduct(product: Product): Long
+
+    @Update
+    fun updateProduct(product: Product): Long
+
+    @Query("SELECT * FROM tbl_product ORDER BY product_name ASC")
+    fun getAllProducts(): List<Product>
+}
