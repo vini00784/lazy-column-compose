a8432413ea3fc9e6fafc3492930e2968c4dab5b7
vini00784 <vo70346@gmail.com>
2023-03-02 07:35:49 -0300

    creating state variables and application layout

--- app/src/main/java/br/senai/sp/jandira/productlistds3m/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/productlistds3m/MainActivity.kt
@@ -1,23 +1,27 @@
 package br.senai.sp.jandira.productlistds3m
 
 import android.os.Bundle
+import android.widget.Space
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.InspectableModifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.productlistds3m.components.CreateProductCard
 import br.senai.sp.jandira.productlistds3m.dao.repository.ProductRepository
+import br.senai.sp.jandira.productlistds3m.model.Product
 import br.senai.sp.jandira.productlistds3m.ui.theme.ProductListDS3MTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,10 +43,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
+
+    var productNameState by remember {
+        mutableStateOf("")
+    }
+
+    var productPriceState by remember {
+        mutableStateOf("")
+    }
+
+    var productDescriptionState by remember {
+        mutableStateOf("")
+    }
+
+    var productsListState by remember {
+        mutableStateOf(listOf<Product>())
+    }
+
+    val context = LocalContext.current
+
+    val productRepository = ProductRepository(context)
+
+    productsListState = productRepository.getProducts()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = 8.dp),
+            .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
@@ -50,10 +77,74 @@ fun Greeting(name: String) {
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold
         )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Product's Name",
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Start
+        )
+
+        OutlinedTextField(
+            value = productNameState,
+            onValueChange = {
+                productNameState = it
+            },
+            modifier = Modifier.fillMaxWidth(),
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Product's Description",
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Start
+        )
+
+        OutlinedTextField(
+            value = productDescriptionState,
+            onValueChange = {
+                productDescriptionState = it
+            },
+            modifier = Modifier.fillMaxWidth(),
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Product's Price",
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Start
+        )
+
+        OutlinedTextField(
+            value = productPriceState,
+            onValueChange = {
+                productPriceState = it
+            },
+            modifier = Modifier.fillMaxWidth(),
+        )
+
+        Button(
+            onClick = {
+                val p = Product(
+                    productName = productNameState,
+                    productPrice = productPriceState.toDouble(),
+                    productDescription = productDescriptionState
+                )
+                val idNewProduct = productRepository.save(p)
+
+                Toast.makeText(context, "$idNewProduct", Toast.LENGTH_SHORT).show()
+            }
+        ) {
+            Text(text = "Save")
+        }
+
         LazyColumn(
             modifier = Modifier.padding(16.dp)
         ) {
-            items(ProductRepository.getProducts()) {
+            items(productsListState) {
                 CreateProductCard(product = it)
             }
         }
